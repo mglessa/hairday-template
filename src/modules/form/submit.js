bfade3ba05a7c45ea3f998dc88d9f5e47925392c
Matheus Lessa <mglessa.dev@gmail.com>
2024-12-05 09:41:18 -0300

    Atualiza a lista de agendamentos e limpa o formulário após o agendamento

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from 'dayjs';
 import { scheduleNew } from '../../services/schedule-new.js'
+import { schedulesDay } from '../schedules/load.js'
 
 const form = document.querySelector('form');
 const clientName = document.querySelector('#client')
@@ -39,7 +40,14 @@ form.onsubmit = async (event) => {
     // Gera um ID
     const id = new Date().getTime();
 
+    // Faz o agendamento
     await scheduleNew({ id, name, when });
+
+    // Recarrega os agendamentos
+    await schedulesDay();
+
+    // Limpa o input de nome
+    clientName.value = '';
 
   } catch (error) {
     alert('Não foi possível realizar o agendamento.');
